cb0d9a2408b899fec60e788811adeaac9a72a2c9
Lưu Đức Trí <triluu@Luus-MacBook-Air.local>
2022-02-08 10:55:00 +0200

    adding phonebook_backend exercise

---
+++ part 3/phonebook_backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    { 
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    { 
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const number = persons.length
+    response.send("<p>Phonebook has info for 4 people</p>")
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
