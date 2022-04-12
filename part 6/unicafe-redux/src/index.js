3cd0fb5328cfcac0c672f2558845a400f3d7f857
Lưu Đức Trí <triluu@Luus-MacBook-Air.local>
2022-04-12 10:09:54 +0300

    exercise 6.1

--- part 6/unicafe-redux/src/index.js
+++ part 6/unicafe-redux/src/index.js
@@ -6,27 +6,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
-  return (
-    <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
-    </div>
-  )
+  	return (
+    	<div>
+      		<button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button> 
+      		<button onClick={() => store.dispatch({ type: 'OK' })}>ok</button> 
+      		<button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      		<button onClick={() => store.dispatch({ type: 'ZERO' })}>reset stats</button>
+      		<div>good {store.getState().good}</div>
+      		<div>ok {store.getState().ok}</div>
+      		<div>bad {store.getState().bad}</div>
+    	</div>
+  	)
 }
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  	ReactDOM.render(<App />, document.getElementById('root'))
 }
 
 renderApp()
