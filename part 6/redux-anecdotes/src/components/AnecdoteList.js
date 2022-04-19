3c689cc7c9ff8d26eaaf628b2d2ff671766990e5
Lưu Đức Trí <triluu@Luus-MacBook-Air.local>
2022-04-19 10:39:05 +0300

    exercise 6.11

--- part 6/redux-anecdotes/src/components/AnecdoteList.js
+++ part 6/redux-anecdotes/src/components/AnecdoteList.js
@@ -6,6 +6,18 @@ const AnecdoteList = () => {
     const dispatch = useDispatch()
     const anecdotes = useSelector(state => state.anecdotes)
 
+	const handleVote = (anecdote) => {
+		dispatch(vote(anecdote.id))
+		const action = {
+			type: 'notification/notify',
+			payload: `you voted '${anecdote.content}'`
+		}
+		dispatch(action)
+		setTimeout(() => {
+			dispatch({ type: 'notification/notify', payload: null })
+		}, 5000)
+	}
+
     return (
         <div>
       		{anecdotes.map(anecdote =>
@@ -15,7 +27,7 @@ const AnecdoteList = () => {
           			</div>
           			<div>
             			has {anecdote.votes}
-            			<button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+            			<button onClick={() => handleVote(anecdote)}>vote</button>
           			</div>
         		</div>
       		)}
--- part 6/redux-anecdotes/src/components/Notification.js
+++ part 6/redux-anecdotes/src/components/Notification.js
@@ -2,13 +2,17 @@ import { useSelector } from "react-redux"
 
 const Notification = () => {
 	const notification = useSelector(state => state.notification)
+	if (notification === null) {
+		return null
+	}
+
   	const style = {
     	border: 'solid',
     	padding: 10,
     	borderWidth: 1
   	}
 	
-	
+ 
   	return (
     	<div style={style}>
       		{notification}
