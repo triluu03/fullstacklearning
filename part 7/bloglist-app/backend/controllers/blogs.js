69bb84868f3866920f797e5eef08cb4fefa9e510
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-05-12 09:33:13 +0300

    adding backend bloglist app

---
+++ part 7/bloglist-app/backend/controllers/blogs.js
@@ -0,0 +1,69 @@
+const router = require('express').Router()
+
+const Blog = require('../models/blog')
+
+
+// Getting blogs from the server
+router.get('/', async (request, response) => {
+    const blogs = await Blog
+        .find({})
+        .find({}).populate('user', { username: 1, name: 1 })
+
+    response.json(blogs)
+})
+
+
+// Creating new blog
+router.post('/', async (request, response) => {
+    if (!request.user) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = request.user
+    const blog = new Blog({ ...request.body, user: user.id })
+
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+})
+
+
+// Deleting blogs
+router.delete('/:id', async (request, response) => {
+    const blogToDelete = await Blog.findById(request.params.id)
+    if (!blogToDelete ) {
+        return response.status(204).end()
+    }
+
+    if ( blogToDelete.user && blogToDelete.user.toString() !== request.user.id ) {
+        return response.status(401).json({
+            error: 'only the creator can delete a blog'
+        })
+    }
+
+    await Blog.findByIdAndRemove(request.params.id)
+
+    response.status(204).end()
+})
+
+
+// Changing a blog
+router.put('/:id', async (request, response) => {
+    const blog = request.body
+
+    const updatedBlog = await Blog
+        .findByIdAndUpdate(
+            request.params.id,
+            blog,
+            { new: true, runValidators: true, context: 'query' }
+        )
+
+    response.json(updatedBlog)
+})
+
+
+
+module.exports = router
---
+++ part 7/bloglist-app/backend/controllers/users.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcrypt')
+const router = require('express').Router()
+const User = require('../models/user')
+
+// Getting the users
+router.get('/', async (request, response) => {
+    const users = await User
+        .find({})
+        .populate('blogs', { author: 1, title: 1, url: 1, likes: 1 })
+
+    response.json(users)
+})
+
+
+// Creating new users
+router.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    if (!password || password.length<3) {
+        return response.status(400).json({
+            error: 'invalid password'
+        })
+    }
+
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+        return response.status(400).json({
+            error: 'username must be unique'
+        })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+
+module.exports = router
