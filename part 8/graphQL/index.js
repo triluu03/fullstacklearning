92877c232b273a06ad0e3a3b6452a6406b6e5186
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-07-10 14:18:50 +0700

    exercise 8.7

--- part 8/graphQL/index.js
+++ part 8/graphQL/index.js
@@ -113,6 +113,7 @@ const typeDefs = gql`
             published: Int!
             genres: [String!]!
         ): Book
+        editAuthor(name: String!, setBornTo: Int!): Author
     }
 `
 
@@ -157,6 +158,18 @@ const resolvers = {
                 authors = authors.concat(author)
             }
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find((a) => a.name === args.name)
+            if (!author) {
+                return null
+            }
+
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map((a) =>
+                a.name === args.name ? updatedAuthor : a
+            )
+            return updatedAuthor
         },
     },
 }
