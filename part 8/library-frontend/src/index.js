aa635175d91fa9cc18a596090a38b5a91862a7b7
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-07-29 10:31:22 +0300

    exercise 8.8

--- part 8/library-frontend/src/index.js
+++ part 8/library-frontend/src/index.js
@@ -2,4 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {
+    ApolloClient,
+    HttpLink,
+    InMemoryCache,
+    gql,
+    ApolloProvider,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'http://localhost:4000',
+    }),
+})
+
+const query = gql`
+    query {
+        allAuthors {
+            name
+            born
+            bookCount
+        }
+    }
+`
+
+client.query({ query }).then((response) => {
+    console.log(response.data)
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
