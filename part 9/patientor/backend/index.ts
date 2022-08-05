3fa474a1168b663310e1121301776ad5ec8abd83
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-08-05 14:38:30 +0300

    exercise 9.9

--- part 9/patientor/backend/index.ts
+++ part 9/patientor/backend/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
